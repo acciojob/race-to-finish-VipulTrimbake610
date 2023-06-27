5dbdc123c451842de1a3ebe553258534156ffdcd
Vipul Trimbake <130178066+VipulTrimbake610@users.noreply.github.com>
2023-06-27 07:30:15 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
-window.promises = [];
 
+let a = new Promise(function(resolve){
+	setTimeout(function(){
+		resolve("a");
+	},1000)
+});
+let b = new Promise(function(resolve){
+	setTimeout(function(){
+		resolve("b");
+	},2000)
+});
+let c = new Promise(function(resolve){
+	setTimeout(function(){
+		resolve("c");
+	},3000)
+});
+let d = new Promise(function(resolve){
+	setTimeout(function(){
+		resolve("d");
+	},4000)
+});
+let e = new Promise(function(resolve){
+	setTimeout(function(){
+		resolve("e");
+	},5000)
+});
+let x = Promise.any([a,b,c,d,e]);
+x.then(function(data){
+	const div = document.getElementById("output");
+	div.innerText = data;
+	
+})
 // Do not change the code above this
 // add your promises to the array `promises`
+
+window.promises = [a,b,c,d,e];
